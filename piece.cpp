bd53df2def8873e705543e2880bf287980bb9a7f
Jay Brooks <Jay.Brooks@cambridge-design.com>
2022-02-19 11:56:48 +0000

    added contructors for piece and its children classes

--- piece.cpp
+++ piece.cpp
@@ -2,6 +2,15 @@
 #include "piece.hpp"
 #include "definitions.hpp"
 
+
+/**
+ * Does having seperate constructors for each Piece make sense? I guess so because this keeps the definitions all in one place, the objects creating 
+ * each piece only need to know what Piece they want, not all of the details of these pieces (which they would do if we only had a single Piece class 
+ * and specified their details via the contructors) 
+ * Additionally, having each piece as a seperate class means that we can specifiy different movement and attack patterns for them 
+ * 
+ */
+
 namespace piece_ns{
 
 typedef enum Team_t
@@ -53,7 +62,7 @@ class King : Piece
     {
         this->type = piece_ns::king;
         this->value = KING_VALUE;
-        this->fenSymbol = (piece_ns::white == this->team) ? : 'k'
+        this->fenSymbol = (piece_ns::white == this->team) ? WHITE_KING_SYMBOL : BLACK_KING_SYMBOL;
     }
     private:
     
@@ -62,7 +71,12 @@ class King : Piece
 class Queen : Piece
 {
     public:
-
+    Queen(piece_ns::Team_t team) : Piece(team)
+    {
+        this->type = piece_ns::queen;
+        this->value = QUEEN_VALUE;
+        this->fenSymbol = (piece_ns::white == this->team) ? WHITE_QUEEN_SYMBOL : BLACK_QUEEN_SYMBOL;
+    }
     private:
     
 };
@@ -70,7 +84,12 @@ class Queen : Piece
 class Rook : Piece 
 {
     public:
-
+    Rook(piece_ns::Team_t team) : Piece(team)
+    {
+        this->type = piece_ns::rook;
+        this->value = ROOK_VALUE;
+        this->fenSymbol = (piece_ns::white == this->team) ? WHITE_ROOK_SYMBOL : BLACK_ROOK_SYMBOL;
+    }
     private:
     
 };
@@ -78,7 +97,12 @@ class Rook : Piece
 class Bishop : Piece
 {
     public:
-
+    Bishop(piece_ns::Team_t team) : Piece(team)
+    {
+        this->type = piece_ns::bishop;
+        this->value = BISHOP_VALUE;
+        this->fenSymbol = (piece_ns::white == this->team) ? WHITE_BISHOP_SYMBOL : BLACK_BISHOP_SYMBOL;
+    }
     private:
     
 };
@@ -86,7 +110,12 @@ class Bishop : Piece
 class Knight : Piece 
 {
     public:
-
+    Knight(piece_ns::Team_t team) : Piece(team)
+    {
+        this->type = piece_ns::knight;
+        this->value = KNIGHT_VALUE;
+        this->fenSymbol = (piece_ns::white == this->team) ? WHITE_KNIGHT_SYMBOL : BLACK_KNIGHT_SYMBOL;
+    }
     private:
     
 };
@@ -94,7 +123,12 @@ class Knight : Piece
 class Pawn : Piece 
 {
     public:
-
+    Pawn(piece_ns::Team_t team) : Piece(team)
+    {
+        this->type = piece_ns::pawn;
+        this->value = PAWN_VALUE;
+        this->fenSymbol = (piece_ns::white == this->team) ? WHITE_PAWN_SYMBOL : BLACK_PAWN_SYMBOL;
+    }
     private:
     
 };
